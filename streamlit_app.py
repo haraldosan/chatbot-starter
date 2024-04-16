6029df26284d7d17849697e652ef59fb4aa00836
Harald Osan <67067769+haraldosan@users.noreply.github.com>
2024-04-16 15:54:38 +0000

    Added source CV, changed to document based conversational langchain model, added requirements

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,28 @@
 # Import required libraries
 from itertools import zip_longest
 import streamlit as st
+from langchain_community.document_loaders import PyPDFLoader
 from streamlit_chat import message
-
-from langchain.chat_models import ChatOpenAI
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.schema import (
     SystemMessage,
     HumanMessage,
     AIMessage
 )
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.vectorstores import FAISS
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain.chains import create_history_aware_retriever
+from langchain_core.prompts import MessagesPlaceholder
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+
 #Load environment variables
 # Set streamlit page configuration
 st.set_page_config(page_title="AI-Kurs ChatBot")
@@ -30,7 +44,36 @@ chat = ChatOpenAI(
     model_name="gpt-3.5-turbo",
     api_key=st.secrets['openai_api_key']
 )
+# Intialize embeddings
+embeddings = OpenAIEmbeddings()
 
+#Fetch and load document
+path = "./docs/CV.pdf"
+loader = PyPDFLoader(path)
+docs = loader.load()
+
+#Indexing
+text_splitter = RecursiveCharacterTextSplitter()
+documents = text_splitter.split_documents(docs)
+vector = FAISS.from_documents(documents, embeddings)
+
+#Set retriever and create retrieval chain
+retriever = vector.as_retriever()
+prompt = ChatPromptTemplate.from_messages([
+    MessagesPlaceholder(variable_name="chat_history"),
+    ("user", "{input}"),
+    ("user", "Given the above conversation, generate a search query to look up to get information relevant to the conversation")
+])
+retriever_chain = create_history_aware_retriever(chat, retriever, prompt)
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "Answer the user's questions based on the below context:\n\n{context}"),
+    MessagesPlaceholder(variable_name="chat_history"),
+    ("user", "{input}"),
+])
+
+document_chain = create_stuff_documents_chain(chat, prompt)
+retrieval_chain = create_retrieval_chain(retriever_chain, document_chain)
 
 def build_message_list():
     """
@@ -52,17 +95,15 @@ def build_message_list():
     return zipped_messages
 
 
-def generate_response():
+def generate_response(user_input):
     """
     Generate AI response using the ChatOpenAI model.
     """
     # Build the list of messages
     zipped_messages = build_message_list()
-
+    output = retrieval_chain.invoke({"chat_history":zipped_messages,"input":user_input})
     # Generate response using the chat model
-    ai_response = chat(zipped_messages)
-
-    return ai_response.content
+    return output['answer']
 
 
 # Define function to submit user input
@@ -81,11 +122,11 @@ if st.session_state.entered_prompt != "":
     # Get user query
     user_query = st.session_state.entered_prompt
 
+    # Generate response
+    output = generate_response(user_query)
+
     # Append user query to past queries
     st.session_state.past.append(user_query)
-
-    # Generate response
-    output = generate_response()
 
     # Append AI response to generated responses
     st.session_state.generated.append(output)
