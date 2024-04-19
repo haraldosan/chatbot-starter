33a82d49271dd5ea5e126f18a640cba594915f2a
Harald Osan <67067769+haraldosan@users.noreply.github.com>
2024-04-19 12:21:08 +0000

    Added sidebar and user input to said sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ from itertools import zip_longest
 import streamlit as st
 from langchain_community.document_loaders import PyPDFLoader
 from streamlit_chat import message
-from langchain_openai import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings # type: ignore
 from langchain_community.vectorstores import FAISS
 from langchain.schema import (
     SystemMessage,
@@ -14,11 +14,11 @@ import os
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import FAISS
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings # type: ignore
 from langchain.chains import create_history_aware_retriever
 from langchain_core.prompts import MessagesPlaceholder
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_openai import ChatOpenAI
+from langchain_openai import ChatOpenAI # type: ignore
 from langchain_core.messages import HumanMessage, AIMessage
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
@@ -40,43 +40,51 @@ if 'past' not in st.session_state:
 if 'entered_prompt' not in st.session_state:
     st.session_state['entered_prompt'] = ""  # Store the latest user input
 
+if 'temperature' not in st.session_state:
+    st.session_state['temperature'] = 0.1 #Store user input for AI bot temperature   
+
 # Initialize the ChatOpenAI model
-chat = ChatOpenAI(
-    temperature=0.1,
-    model_name="gpt-4-turbo",
-    api_key=st.secrets['openai_api_key']
-)
+
+def initialize_openai_bot():
+    return ChatOpenAI(
+        temperature=st.session_state['temperature'],
+        model_name="gpt-4-turbo",
+        api_key=st.secrets['openai_api_key']
+    )
 # Intialize embeddings
 embeddings = OpenAIEmbeddings(openai_api_key=st.secrets['openai_api_key'])
 
 #Fetch and load documents
-path = "https://if.no/apps/vilkarsbasendokument/Vilkaar?produkt=Generelle_vilk%C3%A5r"
-loader = PyPDFLoader(path)
-docs = loader.load()
 
-#Indexing
-text_splitter = RecursiveCharacterTextSplitter()
-documents = text_splitter.split_documents(docs)
-#documents = 
-vector = FAISS.from_documents(documents, embeddings)
+def build_retriever():
+    path = "https://if.no/apps/vilkarsbasendokument/Vilkaar?produkt=Generelle_vilk%C3%A5r"
+    loader = PyPDFLoader(path)
+    docs = loader.load()
 
-#Set retriever and create retrieval chain
-retriever = vector.as_retriever()
-prompt = ChatPromptTemplate.from_messages([
-    MessagesPlaceholder(variable_name="chat_history"),
-    ("user", "{input}"),
-    ("user", "Gitt den tidligere samtalen, generer et søk som gir mening i relasjon til tidligere samtale")
-])
-retriever_chain = create_history_aware_retriever(chat, retriever, prompt)
+    #Indexing
+    text_splitter = RecursiveCharacterTextSplitter()
+    documents = text_splitter.split_documents(docs)
+    #documents = 
+    vector = FAISS.from_documents(documents, embeddings)
 
-prompt = ChatPromptTemplate.from_messages([
-    ("system", "Svar på brukerens spørsmål som en ekspert innen forsikring og vilkår:\n\n{context}"),
-    MessagesPlaceholder(variable_name="chat_history"),
-    ("user", "{input}"),
-])
+    #Set retriever and create retrieval chain
+    retriever = vector.as_retriever()
+    prompt = ChatPromptTemplate.from_messages([
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("user", "{input}"),
+        ("user", "Gitt den tidligere samtalen, generer et søk som gir mening i relasjon til tidligere samtale")
+    ])
+    retriever_chain = create_history_aware_retriever(chat, retriever, prompt)
 
-document_chain = create_stuff_documents_chain(chat, prompt)
-retrieval_chain = create_retrieval_chain(retriever_chain, document_chain)
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", "Svar på brukerens spørsmål som en ekspert innen forsikring og vilkår:\n\n{context}"),
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("user", "{input}"),
+    ])
+
+    document_chain = create_stuff_documents_chain(chat, prompt)
+    retrieval_chain = create_retrieval_chain(retriever_chain, document_chain)
+    return retrieval_chain
 
 def build_message_list():
     """
@@ -115,7 +123,11 @@ def submit():
     st.session_state.entered_prompt = st.session_state.prompt_input
     # Clear prompt_input
     st.session_state.prompt_input = ""
-
+    
+def change_temp():
+    # Set entered_prompt to the current value of prompt_input
+    st.session_state.temperature = st.session_state.prompt_temp
+    chat = initialize_openai_bot()
 
 # Create a text input for user
 st.text_input('YOU: ', key='prompt_input', on_change=submit)
@@ -143,6 +155,12 @@ if st.session_state['generated']:
         message(st.session_state['past'][i],
                 is_user=True, key=str(i) + '_user')
 
+with st.sidebar:
+    pdf_url = st.text_input(default=path,label="Legg til URL til PDF")
+    st.slider(min_value=0,max_value=1,label="Juster temperaturen til AI",step=0.1,key="prompt_temp", on_change=change_temp)
+
+chat = initialize_openai_bot()
+retrieval_chain = build_retriever()
 
 # Add credit
 st.markdown("""
